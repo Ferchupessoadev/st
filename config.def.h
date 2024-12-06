a35908c57a36b90c64bbfe69fafc2e560c410acc
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2024-12-06 00:14:09 -0300

    fix: font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-"JetBrains Mono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font =
+    "JetBrains Mono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
